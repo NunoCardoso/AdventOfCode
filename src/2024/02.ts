36e3dce4fbedae56c1c920a8b8f93c7959488450
Nuno <nuno.cardoso@cegal.com>
2024-12-02 08:12:35 +0100

    2024 02

---
+++ src/2024/02.ts
@@ -0,0 +1,24 @@
+export default {
+  config: {
+    year: '2024',
+    day: '02',
+    title: 'Red-Nosed Reports',
+    status: 'done',
+    comment: 'Nice, simple check while reading input',
+    difficulty: 1
+  },
+  logLevel: 'debug',
+  test: {
+    id: 'test',
+    answers: {
+      part1: 2,
+      part2: 4
+    }
+  },
+  prod: {
+    answers: {
+      part1: 472,
+      part2: 520
+    }
+  }
+}
