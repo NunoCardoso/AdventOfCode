d504feb4e5a7ebcfa55f7e10e74f13996ab4bc7b
Nuno <nuno.cardoso@cegal.com>
2024-12-04 08:11:01 +0100

    2024 04

---
+++ src/2024/04.ts
@@ -0,0 +1,24 @@
+export default {
+  config: {
+    year: '2024',
+    day: '04',
+    title: 'Ceres Search',
+    status: 'inprogress',
+    comment: '',
+    difficulty: 0
+  },
+  logLevel: 'debug',
+  test: {
+    id: 'test',
+    answers: {
+      part1: 18,
+      part2: 9
+    }
+  },
+  prod: {
+    answers: {
+      part1: 2603,
+      part2: 1965
+    }
+  }
+}
