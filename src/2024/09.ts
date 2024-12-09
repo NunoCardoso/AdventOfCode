78782f17d5251fe7f095718bb536e7905ae9c37c
Nuno <nuno.cardoso@cegal.com>
2024-12-09 10:36:22 +0100

    2024 09

---
+++ src/2024/09.ts
@@ -0,0 +1,24 @@
+export default {
+  config: {
+    year: '2024',
+    day: '09',
+    title: '',
+    status: 'inprogress',
+    comment: '',
+    difficulty: 0
+  },
+  logLevel: 'debug',
+  test: {
+    id: 'test',
+    answers: {
+      part1: 1928,
+      part2: 2858
+    }
+  },
+  prod: {
+    answers: {
+      part1: 6330095022244,
+      part2: 6359491814941
+    }
+  }
+}
