7a3531c531d0448f48297bc9624b8b310da473f7
Nuno <nuno.cardoso@cegal.com>
2024-12-15 10:39:00 +0100

    2024 15

---
+++ src/2024/15.solution.ts
@@ -0,0 +1,207 @@
+import { Params } from 'aoc.d'
+import clc from 'cli-color'
+import { Point, PointPlus, World } from 'declarations'
+import { waitForKey } from 'util/promise'
+
+export default async (lineReader: any, params: Params) => {
+  const log = require('console-log-level')({ level: params.logLevel ?? 'info' })
+
+  let part1: number = 0
+  let part2: number = 0
+
+  let worldPart1: World<string> = []
+  let startPart1: Point = [0, 0]
+  let worldPart2: World<string> = []
+  let startPart2: Point = [0, 0]
+
+  let rowIndex = 0
+  let instructions: string[] = []
+  for await (const line of lineReader) {
+    const values: string[] = line?.split('')
+    worldPart1[rowIndex] = []
+    worldPart2[rowIndex] = []
+    if (values.length > 0) {
+      if (values[0] === '#') {
+        values.forEach((val, colIndex) => {
+          worldPart1[rowIndex].push(val)
+          worldPart2[rowIndex].push(val === 'O' ? '[' : val)
+          worldPart2[rowIndex].push(val === 'O' ? ']' : val)
+          if (val === '@') {
+            startPart1 = [rowIndex, colIndex]
+            startPart2 = [rowIndex, colIndex * 2]
+            worldPart1[rowIndex][colIndex] = '.'
+            worldPart2[rowIndex][colIndex * 2] = '.'
+            worldPart2[rowIndex][colIndex * 2 + 1] = '.'
+          }
+        })
+        rowIndex++
+      } else {
+        instructions = instructions.concat(values)
+      }
+    }
+  }
+
+  const calculateScore = (world: World<string>): number =>
+    world.reduce((acc, row, rowIndex) => {
+      return (
+        acc +
+        row.reduce((acc2, cell, colIndex) => {
+          return acc2 + (['O', '['].includes(cell) ? 100 * rowIndex + colIndex : 0)
+        }, 0)
+      )
+    }, 0)
+
+  const printWorld = (world: World<string>, start: Point) => {
+    for (var i = 0; i < world.length; i++) {
+      let s: string = ''
+      for (var j = 0; j < world[i].length; j++) {
+        if (i === start[0] && j === start[1]) {
+          s += clc.yellow('@')
+          continue
+        }
+        let cell = world[i][j]
+        if (cell === '#') s += clc.red(cell)
+        if (['O', '[', ']'].includes(cell)) s += clc.cyan(cell)
+        if (cell === '.') s += cell
+      }
+      log.info(s)
+    }
+  }
+
+  const executePath = (world: World<string>, current: Point, path: PointPlus<string>[] | null): Point => {
+    if (!path) return current
+    let lastPlace: Point
+    path.reverse().forEach((p) => {
+      if (p[2] === '^') {
+        world[p[0] - 1][p[1]] = world[p[0]][p[1]]
+        lastPlace = [p[0] - 1, p[1]]
+      }
+      if (p[2] === 'v') {
+        world[p[0] + 1][p[1]] = world[p[0]][p[1]]
+        lastPlace = [p[0] + 1, p[1]]
+      }
+      if (p[2] === '<') {
+        world[p[0]][p[1] - 1] = world[p[0]][p[1]]
+        lastPlace = [p[0], p[1] - 1]
+      }
+      if (p[2] === '>') {
+        world[p[0]][p[1] + 1] = world[p[0]][p[1]]
+        lastPlace = [p[0], p[1] + 1]
+      }
+      world[p[0]][p[1]] = '.'
+    })
+    return lastPlace!
+  }
+
+  const checkIfRobotCanMove = (
+    world: World<string>,
+    current: Point,
+    instruction: string,
+    path: PointPlus<string>[]
+  ): PointPlus<string>[] | null => {
+    let targetedPoint: Point = [0, 0]
+
+    if (instruction === '^') targetedPoint = [current[0] - 1, current[1]] as Point
+    if (instruction === 'v') targetedPoint = [current[0] + 1, current[1]] as Point
+    if (instruction === '<') targetedPoint = [current[0], current[1] - 1] as Point
+    if (instruction === '>') targetedPoint = [current[0], current[1] + 1] as Point
+    let targetWorld = world[targetedPoint[0]][targetedPoint[1]]
+    if (targetWorld === '.') {
+      return path.concat([[...current, instruction]])
+    }
+    if (targetWorld === '#') {
+      return null
+    }
+    if (targetWorld === 'O') {
+      return checkIfRobotCanMove(world, targetedPoint, instruction, path.concat([[...current, instruction]]))
+    }
+    if (targetWorld === '[') {
+      let otherTarget = [targetedPoint[0], targetedPoint[1] + 1] as Point
+      if (['^', 'v'].includes(instruction)) {
+        let leftPath = checkIfRobotCanMove(
+          world,
+          targetedPoint,
+          instruction,
+          path.concat([[...current, instruction]])
+        )
+
+        let rightPath = checkIfRobotCanMove(world, otherTarget, instruction, path)
+        if (leftPath === null || rightPath === null) return null
+        return leftPath.concat(rightPath)
+      }
+      // we are in >
+      return checkIfRobotCanMove(
+        world,
+        otherTarget,
+        instruction,
+        path.concat([
+          [...current, instruction],
+          [...targetedPoint, instruction]
+        ])
+      )
+    }
+    if (targetWorld === ']') {
+      let otherTarget = [targetedPoint[0], targetedPoint[1] - 1] as Point
+      if (['^', 'v'].includes(instruction)) {
+        let rightPath = checkIfRobotCanMove(
+          world,
+          targetedPoint,
+          instruction,
+          path.concat([[...current, instruction]])
+        )
+        let leftPath = checkIfRobotCanMove(world, otherTarget, instruction, path)
+        if (!leftPath || !rightPath) return null
+        return rightPath.concat(leftPath)
+      }
+      // we are in <
+      return checkIfRobotCanMove(
+        world,
+        otherTarget,
+        instruction,
+        path.concat([
+          [...current, instruction],
+          [...targetedPoint, instruction]
+        ])
+      )
+    }
+    return null
+  }
+
+  const sortPath = (path: PointPlus<string>[], instruction: string): PointPlus<string>[] => {
+    let path2: PointPlus<string>[] = []
+    path?.forEach((p) => {
+      if (!path2.find((_p) => _p[0] === p[0] && _p[1] === p[1] && _p[2] === p[2])) path2.push(p)
+    })
+    if (instruction === '^')
+      path2 = path2.sort((a, b) => (b[0] - a[0] < 0 ? -1 : b[0] - a[0] > 0 ? 1 : b[1] - a[1]))
+    if (instruction === 'v')
+      path2 = path2.sort((a, b) => (a[0] - b[0] < 0 ? -1 : a[0] - b[0] > 0 ? 1 : a[1] - b[1]))
+    if (instruction === '<')
+      path2 = path2.sort((a, b) => (b[1] - a[1] < 0 ? -1 : b[1] - a[1] > 0 ? 1 : b[0] - a[0]))
+    if (instruction === '>')
+      path2 = path2.sort((a, b) => (a[1] - b[1] < 0 ? -1 : a[1] - b[1] > 0 ? 1 : a[0] - b[0]))
+    return path2
+  }
+  const solveFor = async (world: World<string>, current: Point, instructions: string[]): Promise<number> => {
+    while (instructions.length > 0) {
+      let instruction = instructions.shift()!
+      let path: PointPlus<string>[] | null = checkIfRobotCanMove(world, current, instruction, [])
+      if (path !== null) {
+        let path2 = sortPath(path, instruction)
+        current = executePath(world, current, path2)
+        if (params.ui?.show) printWorld(world, current)
+        if (params.ui?.keypress) await waitForKey()
+      }
+    }
+    return calculateScore(world)
+  }
+
+  if (!params.skipPart1) {
+    part1 = await solveFor(worldPart1, startPart1, [...instructions])
+  }
+  if (!params.skipPart2) {
+    part2 = await solveFor(worldPart2, startPart2, [...instructions])
+  }
+
+  return { part1, part2 }
+}
---
+++ src/2024/15.ts
@@ -0,0 +1,42 @@
+export default {
+  config: {
+    year: '2024',
+    day: '15',
+    title: 'Warehouse Woes',
+    status: 'done',
+    comment: 'Hard part2. I only got it as I sorted the paths. A lot of simplifications can be done',
+    difficulty: 4
+  },
+  logLevel: 'debug',
+  test: [
+    {
+      id: 'test1',
+      answers: {
+        part1: 2028
+      }
+    },
+    {
+      id: 'test2',
+      answers: {
+        part1: 10092,
+        part2: 9021
+      }
+    },
+    {
+      id: 'test3',
+      answers: {
+        //part2: 0
+      }
+    }
+  ],
+  ui: {
+    show: false,
+    keypress: false
+  },
+  prod: {
+    answers: {
+      part1: 1527563,
+      part2: 1521635
+    }
+  }
+}
