b6c09be6893c598eac6e55bafc278b51193c1593
Nuno <nuno.cardoso@cegal.com>
2024-12-19 10:18:22 +0100

    2024 19

--- src/2024/19.solution.ts
+++ src/2024/19.solution.ts
@@ -1,8 +1,6 @@
 import { Params } from 'aoc.d'
 
 export default async (lineReader: any, params: Params) => {
-  // const log = require('console-log-level')({ level: params.logLevel ?? 'info' })
-
   let part1: number = 0
   let part2: number = 0
 
@@ -18,7 +16,7 @@ export default async (lineReader: any, params: Params) => {
   const isItPossible = (towel: string, towelPatterns: string[], cache: Record<string, boolean>): boolean => {
     // return cache if found
     if (Object.prototype.hasOwnProperty.call(cache, towel)) return cache[towel]
-    let result = towelPatterns.some((pattern, index, array) => {
+    let result = towelPatterns.some((pattern) => {
       if (towel.startsWith(pattern)) {
         let remaining = towel.substring(pattern.length, towel.length)
         return remaining.length === 0 ? true : isItPossible(remaining, towelPatterns, cache)
@@ -29,18 +27,27 @@ export default async (lineReader: any, params: Params) => {
     return result
   }
 
-  if (!params.skipPart1) {
-    let cache: Record<string, boolean> = {}
-    part1 = newTowels.reduce((acc, towel) => acc + (isItPossible(towel, towels!, cache) ? 1 : 0), 0)
+  const inHowManyWays = (towel: string, towelPatterns: string[], cache: Record<string, number>): number => {
+    // return cache if found
+    if (Object.prototype.hasOwnProperty.call(cache, towel)) return cache[towel]
+    let result = towelPatterns.reduce((acc, pattern) => {
+      if (towel.startsWith(pattern)) {
+        let remaining = towel.substring(pattern.length, towel.length)
+        return acc + (remaining.length === 0 ? 1 : inHowManyWays(remaining, towelPatterns, cache))
+      }
+      return acc
+    }, 0)
+    cache[towel] = result
+    return result
   }
 
-  /*if (!params.skipPart2) {
-    let cache: Record<string, boolean> = {}
-    part2 = newTowels.reduce((acc, towel) => {
-      console.log('trying towel', towel)
-      return acc + (isItPossiblePart2(towel, towels!, cache) ? 1 : 0)
-    }
-  }*/
+  if (!params.skipPart1) {
+    part1 = newTowels.reduce((acc, towel) => acc + (isItPossible(towel, towels!, {}) ? 1 : 0), 0)
+  }
+
+  if (!params.skipPart2) {
+    part2 = newTowels.reduce((acc, towel) => acc + inHowManyWays(towel, towels!, {}), 0)
+  }
 
   return { part1, part2 }
 }
--- src/2024/19.ts
+++ src/2024/19.ts
@@ -11,14 +11,14 @@ export default {
   test: {
     id: 'test',
     answers: {
-      part1: 6
-      //  part2: 16
+      part1: 6,
+      part2: 16
     }
   },
   prod: {
     answers: {
-      part1: 283
-      //part2: 0
+      part1: 283,
+      part2: 615388132411142
     }
   }
 }
