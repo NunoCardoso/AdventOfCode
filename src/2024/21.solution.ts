3d51f90378c0d58d011974f9e62fb80bfcad9624
Nuno <nuno.cardoso@cegal.com>
2024-12-22 19:53:11 +0100

    Finally 2024 21 part 2

--- src/2024/21.solution.ts
+++ src/2024/21.solution.ts
@@ -1,81 +1,70 @@
 import * as console from 'console'
 import { Params } from 'aoc.d'
 
-export const keypadCheat: Record<string, Record<string, string>> = {
-  A: { A: '', '0': '<', '1': '<<^', '2': '<^', '3': '^', '4': '<<^^', '5': '<^^', '6': '^^', '7': '<<^^^', '8': '<^^^', '9': '^^^' },
-  '0': { A: '>', '0': '', '1': '<^', '2': '^', '3': '^>', '4': '<^^', '5': '^^', '6': '^^>', '7': '<^^^', '8': '^^^', '9': '^^^>' },
-  '1': { A: 'v>>', '0': 'v>', '1': '', '2': '>', '3': '>>', '4': '^', '5': '^>', '6': '^>>', '7': '^^', '8': '^^>', '9': '^^>>' },
+export const keypadMoves: Record<string, Record<string, string>> = {
+  A: { A: '', '0': '<', '1': '^<<', '2': '<^', '3': '^', '4': '^^<<', '5': '<^^', '6': '^^', '7': '^^^<<', '8': '<^^^', '9': '^^^' },
+  '0': { A: '>', '0': '', '1': '^<', '2': '^', '3': '^>', '4': '^^<', '5': '^^', '6': '^^>', '7': '^^^<', '8': '^^^', '9': '^^^>' },
+  '1': { A: '>>v', '0': '>v', '1': '', '2': '>', '3': '>>', '4': '^', '5': '^>', '6': '^>>', '7': '^^', '8': '^^>', '9': '^^>>' },
   '2': { A: 'v>', '0': 'v', '1': '<', '2': '', '3': '>', '4': '<^', '5': '^', '6': '^>', '7': '<^^', '8': '^^', '9': '^^>' },
   '3': { A: 'v', '0': '<v', '1': '<<', '2': '<', '3': '', '4': '<<^', '5': '<^', '6': '^', '7': '<<^^', '8': '<^^', '9': '^^' },
-  '4': { A: 'vv>>', '0': 'vv>', '1': 'v', '2': 'v>', '3': 'v>>', '4': '', '5': '>', '6': '>>', '7': '^', '8': '^>', '9': '^>>' },
+  '4': { A: '>>vv', '0': '>vv', '1': 'v', '2': 'v>', '3': 'v>>', '4': '', '5': '>', '6': '>>', '7': '^', '8': '^>', '9': '^>>' },
   '5': { A: 'vv>', '0': 'vv', '1': '<v', '2': 'v', '3': 'v>', '4': '<', '5': '', '6': '>', '7': '<^', '8': '^', '9': '^>' },
   '6': { A: 'vv', '0': '<vv', '1': '<<v', '2': '<v', '3': 'v', '4': '<<', '5': '<', '6': '', '7': '<<^', '8': '<^', '9': '^' },
-  '7': { A: 'vvv>>', '0': 'vvv>', '1': 'vv', '2': 'vv>', '3': 'vv>>', '4': 'v', '5': 'v>', '6': 'v>>', '7': '', '8': '>', '9': '>>' },
+  '7': { A: '>>vvv', '0': '>vvv', '1': 'vv', '2': 'vv>', '3': 'vv>>', '4': 'v', '5': 'v>', '6': 'v>>', '7': '', '8': '>', '9': '>>' },
   '8': { A: 'vvv>', '0': 'vvv', '1': '<vv', '2': 'vv', '3': 'vv>', '4': '<v', '5': 'v', '6': 'v>', '7': '<', '8': '', '9': '>' },
   '9': { A: 'vvv', '0': '<vvv', '1': '<<vv', '2': '<vv', '3': 'vv', '4': '<<v', '5': '<v', '6': 'v', '7': '<<', '8': '<', '9': '' }
 }
 
-export const keyboardCheat: Record<string, Record<string, string>> = {
-  A: { A: '', '<': 'v<<', v: 'v<', '>': 'v', '^': '<' },
+export const keyboardMoves: Record<string, Record<string, string>> = {
+  A: { A: '', '<': 'v<<', v: '<v', '>': 'v', '^': '<' },
   '<': { A: '>>^', '<': '', v: '>', '>': '>>', '^': '>^' },
-  v: { A: '>^', '<': '<', v: '', '>': '>', '^': '^' },
+  v: { A: '^>', '<': '<', v: '', '>': '>', '^': '^' },
   '>': { A: '^', '<': '<<', v: '<', '>': '', '^': '<^' },
   '^': { A: '>', '<': 'v<', v: 'v', '>': 'v>', '^': '' }
 }
 
-export const doKeypadMovesFor = (code: string): string => {
+export const doKeypadMovesFor = (code: string): string[] => {
   let current: string = 'A'
-  return code.split('').reduce((acc, key) => {
-    let x = keypadCheat[current][key]
+  return code.split('').map((key) => {
+    let moves = keypadMoves[current][key]
     current = key
-    return acc + x + 'A'
-  }, '')
+    return moves + 'A'
+  })
 }
 
-export const doKeyboardMovesFor = (code: string): string => {
+export const doKeyboardMovesFor = (code: string, deepness: number, cache: Record<string, number>): number => {
   let current: string = 'A'
-  console.log(code)
-  return code.split('').reduce((acc, key) => {
-    let x = keyboardCheat[current][key]
+  if (deepness === 0) return code.length
+  if (cache[deepness + ',' + code]) return cache[deepness + ',' + code]
+  let pieces = code.split('').reduce((acc, key) => {
+    let moves = keyboardMoves[current][key]
     current = key
-    return acc + x + 'A'
-  }, '')
+    return acc + doKeyboardMovesFor(moves + 'A', deepness - 1, cache)
+  }, 0)
+  cache[deepness + ',' + code] = pieces
+  return pieces
 }
 
 export default async (lineReader: any, params: Params) => {
-  const log = require('console-log-level')({ level: params.logLevel ?? 'info' })
-
   let part1: number = 0
   let part2: number = 0
 
   let codes: string[] = []
   for await (const line of lineReader) codes.push(line)
 
-  const complexity = (code3: string, code: string): number => {
-    let codeNumber = Number(code.replaceAll('A', ''))
-    console.log('complexity', code3, code, codeNumber, code3.length, code3.length * codeNumber)
-    return code3.length * codeNumber
-  }
+  const complexity = (keyboardCodeLength: number, keypadCode: string): number => keyboardCodeLength * Number(keypadCode.replaceAll('A', ''))
 
-  const optimizeCode = (code: string): string => code.replaceAll(/?v<A/g, '<vA').replaceAll('^<A', '<^A').replaceAll('>vA', 'v>A')
-
-  const solveFor = (codes: string[]): number =>
+  const solveFor = (codes: string[], numberOfRobots: number): number =>
     codes.reduce((acc, code) => {
-      let code1 = doKeypadMovesFor(code)
-      let code2 = doKeyboardMovesFor(code1)
-      code2 = optimizeCode(code2)
-      let code3 = doKeyboardMovesFor(code2)
-      // code3 = optimizeCode(code3)
-      console.log('code', code, 'code1', code1, 'code2', code2, 'code3', code3, 'complexity', complexity(code3, code))
-      return acc + complexity(code3, code)
-      // return 1
+      let finalCodeSize: number = doKeypadMovesFor(code).reduce((acc, c) => acc + doKeyboardMovesFor(c, numberOfRobots, {}), 0)
+      return acc + complexity(finalCodeSize, code)
     }, 0)
 
   if (!params.skipPart1) {
-    part1 = solveFor(codes)
+    part1 = solveFor(codes, params.robots.part1)
   }
   if (!params.skipPart2) {
-    part2 = solveFor(codes)
+    part2 = solveFor(codes, params.robots.part2)
   }
 
   return { part1, part2 }
--- src/2024/21.ts
+++ src/2024/21.ts
@@ -3,22 +3,30 @@ export default {
     year: '2024',
     day: '21',
     title: 'Keypad Conundrum',
-    status: 'inprogress',
-    comment: '',
-    difficulty: 0
+    status: 'done',
+    comment:
+      'So far, the best puzzle of the show. See readme for a comprehensive detail. It was really intuitive that ' +
+      'some keypad paths were faster, but not obvious for the keyboard. A bit difficult to cache and optimize, but really ' +
+      'good stuff',
+    difficulty: 5
   },
-  logLevel: 'debug',
+  logLevel: 'info',
   test: {
     id: 'test',
     answers: {
       part1: 126384
-      //part2: 0
+    }
+  },
+  params: {
+    robots: {
+      part1: 2,
+      part2: 25
     }
   },
   prod: {
     answers: {
-      //part1: 0,
-      //part2: 0
+      part1: 105458,
+      part2: 129551515895690
     }
   }
 }
